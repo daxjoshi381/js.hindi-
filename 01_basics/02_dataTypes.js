4c61b75de55ecd11b32a929d8b4864591be06436
Dax joshi <daxjoshi381@gmail.com>
2025-12-16 03:48:31 +0000

    done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+
+console.log(3
+    +
+    3); // code readability should be high
+
+    console.log("dax");
+
+    let name = "dax"
+    let age = 45
+    let isLoggedIn = false
+    let state;
+
+    // number => 2 to power 53 
+    // bigint 
+    // string => ""
+    // boolean => true/false 
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+
+
+    // object 
+
+
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); // object
+    
+    
+    
+    
